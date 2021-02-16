909a78d4c6f18960a0cbe5797f1cbbe229aa25c4
dionis <dionis.rodrigo2@gmail.com>
2021-02-16 19:10:40 -0300

    desafio operafdores

--- Cursos/Java/src/javaBasico/OperadoresAritmeticos.java
+++ Cursos/Java/src/javaBasico/OperadoresAritmeticos.java
@@ -13,6 +13,13 @@ public class OperadoresAritmeticos {
 		System.out.println(a / x);
 		System.out.println(a - x);
 		System.out.println(a % x);
+		
+		System.out.println("------------------------------------");
+		int r = 3 - 4 * 12;
+		double h = Math.pow(r, 2);
+		
+		System.out.println(r);
+		System.out.println(h);
 
 	}
 
